b44aeaffff19303b29bc991e31497bda378921b3
Ivan Havinson <havinsonivan@gmail.com>
2025-04-18 01:43:12 +0300

    step 4 asciinema left

--- diff_generator/scripts/gendiff.py
+++ diff_generator/scripts/gendiff.py
@@ -1,12 +1,15 @@
 from diff_generator.arg_parser import get_filepath, validate_file_ext, read_file
+from diff_generator.diff_tool import process_flat_files
+from diff_generator.formatter import format_stylish
 
 
 def main():
-    args = get_filepath()
-    validate_file_ext(args.first_file, args.second_file)
-    file_1 = read_file(args.first_file)
-    file_2 = read_file(args.second_file)
-
+    first_file, second_file = get_filepath()
+    validate_file_ext(first_file, second_file)
+    file_1 = read_file(first_file)
+    file_2 = read_file(second_file)
+    diff = process_flat_files(file_1, file_2)
+    print(format_stylish(diff))
     return
 
 if __name__ == "__main__":
